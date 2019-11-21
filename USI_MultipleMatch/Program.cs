b5606be4a009f0a527e684ae037525aa21db6ea2
Iwamoto Hiromasa <tanuki13.snow@gmail.com>
2019-11-22 00:48:42 +0900

    makematch

--- USI_MultipleMatch/Program.cs
+++ USI_MultipleMatch/Program.cs
@@ -5,25 +5,31 @@ namespace USI_MultipleMatch
     class Program
     {
         static bool alive;
-        static void Main(string[] args)
+         static List<(uint byoyomi, uint rounds)> matchlist;
+       static void Main(string[] args)
         {
             alive = true;
-            Console.WriteLine("Hello World!");
+            matchlist = new List<(uint byoyomi, uint rounds)>();
+            Console.WriteLine("連続対局プログラム");
             while (alive)
             {
-                Console.Write("command?()");
+                Console.Write("command?(r/m/s/q) > ");
                 switch (Console.ReadLine())
                 {
                     case "resister":
+                    case "r":
                         resister();
                         break;
                     case "makematch":
+                    case "m":
                         makematch();
                         break;
                     case "start":
+                    case "s":
                         start();
                         break;
                     case "quit":
+                    case "q":
                         alive = false;
                         break;
                 }
@@ -34,27 +40,36 @@ namespace USI_MultipleMatch
         {
 
         }
-        static List<(uint byoyomi, uint rounds)> matchlist;
         static void makematch()
         {
+            matchlist.Clear();
             while (true)
             {
                 Console.WriteLine($"match {matchlist.Count+1}");
-                Console.Write("byoyomi?(millisecond) > ");
+                Console.Write("1手の考慮時間?(ミリ秒) > ");
                 uint byo = uint.Parse(Console.ReadLine());
-                Console.Write("rounds?(times) > ");
+                Console.Write("対戦回数? > ");
                 uint times = uint.Parse(Console.ReadLine());
-                matchlist.Add((byo, times));
-
+                Console.Write($"add [{byo}ms,{times}回]?(y/n) > ");
+                if(Console.ReadLine()=="y")
+                    matchlist.Add((byo, times));
                 Console.Write("current matchlist is ");
+                if (matchlist.Count == 0) Console.Write("empty.");
                 foreach(var match in matchlist)
                 {
-                    Console.Write($"[{match.byoyomi}ms,{match.rounds}rounds] ");
+                    Console.Write($"[{match.byoyomi}ms,{match.rounds}回] ");
                 }
-                Console.Write("add match?(y/n) > ");
+                Console.WriteLine(".");
+                Console.Write("continue?(y/n) > ");
                 if (Console.ReadLine()!="y")
                 {
                     Console.WriteLine("resisterd.");
+                    Console.Write("matchlist is ");
+                    foreach (var match in matchlist)
+                    {
+                        Console.Write($"[{match.byoyomi}ms,{match.rounds}回] ");
+                    }
+                    Console.WriteLine(".");
                     break;
                 }
             }
