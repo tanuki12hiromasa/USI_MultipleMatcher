638229e176875bcc6bb65357d9bee8b375592f82
hiromasa_sg <tanuki13.snow@gmail.com>
2019-11-21 13:37:40 +0900

    make functions
    大まかな骨格を作成

--- USI_MultipleMatch/Program.cs
+++ USI_MultipleMatch/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace USI_MultipleMatch
 {
     class Program
     {
+        static bool alive;
         static void Main(string[] args)
         {
+            alive = true;
             Console.WriteLine("Hello World!");
+            while (alive)
+            {
+                Console.Write("command?()");
+                switch (Console.ReadLine())
+                {
+                    case "resister":
+                        resister();
+                        break;
+                    case "makematch":
+                        makematch();
+                        break;
+                    case "start":
+                        start();
+                        break;
+                    case "quit":
+                        alive = false;
+                        break;
+                }
+            }
+        }
+
+        static void resister()
+        {
+
+        }
+        static List<(uint byoyomi, uint rounds)> matchlist;
+        static void makematch()
+        {
+
+        }
+        static void start()
+        {
+
         }
     }
+
+    
 }
