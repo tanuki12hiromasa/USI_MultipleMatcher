889dc2ee83d756421173e0b6b267880bc95e8714
Iwamoto Hiromasa <tanuki13.snow@gmail.com>
2019-11-24 01:04:35 +0900

    対局のセットアップ部分の作成
    設定を読み込んで対局関数に渡す部分

--- USI_MultipleMatch/Program.cs
+++ USI_MultipleMatch/Program.cs
@@ -6,6 +6,10 @@ namespace USI_MultipleMatch
 {
     class Program
     {
+		enum Result
+		{
+			SenteWin,GoteWin,Draw
+		}
         static bool alive;
         static List<(uint byoyomi, uint rounds)> matchlist;
         static void Main(string[] args) {
@@ -133,8 +137,80 @@ namespace USI_MultipleMatch
             }
         }
         static void start() {
-			//A,Bから
-        }
+			if (matchlist.Count <= 0) {
+				Console.WriteLine("error: matchlist not exist");
+				return;
+			}
+			//A,Bの設定を読み込む
+			string a_path, b_path;
+			var a_option = new List<string>();
+			var b_option = new List<string>();
+			try {
+				using (StreamReader readerA = new StreamReader(@"./ PlayerA.txt")) {
+					Console.WriteLine($"playerA: {readerA.ReadLine()}");
+					a_path = readerA.ReadLine();
+					Console.WriteLine(a_path);
+					for (string sline = readerA.ReadLine(); sline != null && sline != ""; sline = readerA.ReadLine()) {
+						a_option.Add(setoptionusi(sline));
+						Console.WriteLine(a_option.Count - 1);
+					}
+				}
+			}
+			catch (Exception e) {
+				Console.WriteLine("error in load playerA setting");
+				Console.WriteLine(e.Message);
+				return;
+			}
+			try {
+				using (StreamReader readerB = new StreamReader(@"./ PlayerB.txt")) {
+					Console.WriteLine($"playerB: {readerB.ReadLine()}");
+					b_path = readerB.ReadLine();
+					Console.WriteLine(b_path);
+					for (string sline = readerB.ReadLine(); sline != null && sline != ""; sline = readerB.ReadLine()) {
+						a_option.Add(setoptionusi(sline));
+						Console.WriteLine(b_option.Count - 1);
+					}
+				}
+			}
+			catch (Exception e) {
+				Console.WriteLine("error in load playerB setting");
+				Console.WriteLine(e.Message);
+				return;
+			}
+			//
+			Console.Write("experiment name? > ");
+			string expname = DateTime.Now.ToString("yyMMddHHmm") + ' ' + Console.ReadLine();
+			//matchlistに沿ってA,Bの先後を入れ替えながら対局させる
+			foreach(var m in matchlist) {
+				for(uint r = 1; r <= m.rounds; r++) {
+					if (r % 2 == 0) {
+						//a先手
+						string matchname = $"{expname} {m.byoyomi} {r} a";
+						match(matchname, m.byoyomi, a_path, a_option, b_path, b_option);
+					}
+					else {
+						//b先手
+						string matchname = $"{expname} {m.byoyomi} {r} b";
+						match(matchname, m.byoyomi, b_path, b_option, a_path, a_option);
+					}
+				}
+			}
+
+		}
+		static string setoptionusi(string settingline) {
+			var token = settingline.Split(' ');
+			return $"setoption name {token[0]} value {token[2]}";
+		}
+		static Result match(string matchName, uint byoyomi, string s_path, List<string> s_option, string g_path, List<string> g_option) {
+			Console.Write($"{matchName} ");
+			using (Process sente = new Process())
+			using (Process gote = new Process()) {
+				
+
+			}
+			return Result.Draw;
+		}
+
     }
 
 
