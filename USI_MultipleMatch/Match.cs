6d6e3d01af42eefbe5e7befb97c8171fe87141a6
hiromasa_sg <tanuki13.snow@gmail.com>
2022-03-19 23:57:44 +0900

    強化学習用のコマンド・処理を追加
    5五将棋版から移植

--- USI_MultipleMatch/Match.cs
+++ USI_MultipleMatch/Match.cs
@@ -12,7 +12,7 @@ namespace USI_MultipleMatch
 	}
 	class Match 
 	{
-		public static Result match(string matchname, uint byoyomi, Player b, Player w, string startusi = "startpos", string kifupath = @"./kifu.txt") {//開始局面のusiはsfen部分から
+		public static Result match(string matchname, uint byoyomi, Player b, Player w, string startusi = "startpos", string kifupath = @"./kifu.txt", int waittime = 100) {//開始局面のusiはsfen部分から
 			while (true) {
 				using (Process sente = new Process())
 				using (Process gote = new Process()) {
@@ -120,12 +120,132 @@ namespace USI_MultipleMatch
 						if(!gote.HasExited) gote.StandardInput.WriteLine("quit");
 					}
 					finally {
-						if (!sente.WaitForExit(100)) {
+						if (!sente.WaitForExit(waittime)) {
 							try {
 								sente.Kill();
 							}
 							catch (Exception)				
 							{
+								//プロセスは既に終了しているので何もしない（waitが終わってkillを呼ぶまでの一瞬の間にプロセスが終了した場合に例外が発生する）
+							}
+						}
+						if (!gote.WaitForExit(waittime)) {
+							try {
+								gote.Kill();
+							}
+							catch (Exception) {
+								//プロセスは既に終了しているので何もしない
+							}
+						}
+					}
+				}
+			}
+		}
+		public static Result match(string matchname, uint byoyomi, Player b, Player w, out List<string> _kifu, out List<int> _evals, string startusi = "startpos", string kifupath = @"./kifu.txt") {
+
+			while (true) {
+				using (Process sente = new Process())
+				using (Process gote = new Process()) {
+					try {
+						Console.Write($"waiting setup {b.name}...");
+						b.Start(sente);
+						Console.WriteLine(" readyok.");
+						Console.Write($"waiting setup {w.name}...");
+						w.Start(gote);
+						Console.WriteLine(" readyok.");
+						var kifu = new List<string>();
+						var evals = new List<int>();
+						int startmove = 1;
+						List<Kyokumen> history = new List<Kyokumen>();
+						string go = $"go btime 0 wtime 0 byoyomi {byoyomi}";
+						var starttime = DateTime.Now;
+						string startsfen = "startpos";
+						if (startusi != "startpos") {
+							string[] tokens = startusi.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+							if (tokens[0] != "startpos") throw new NotImplementedException();
+							else {
+								history.Add(new Kyokumen());
+								for (int i = 2; i < tokens.Length; i++) {
+									kifu.Add(tokens[i]);
+									evals.Add(0);
+									history.Add(new Kyokumen(history[history.Count - 1], tokens[i]));
+								}
+								startmove = history.Count;
+							}
+						}
+						else {
+							history.Add(new Kyokumen());
+						}
+						Console.WriteLine($"start at {startusi}");
+						Console.Write($"{starttime.ToString(Kifu.TimeFormat)} {matchname}:");
+						Result result;
+						while (true) {
+							if (kifu.Count % 2 == 0) {//先手
+								var (move, eval) = GetMove(sente, position(startsfen, kifu), go);
+								Console.Write($" b:{move}({eval})");
+								if (move == "resign") {
+									result = Result.GoteWin;
+									break;
+								}
+								else if (move == "win") {
+									result = Result.SenteWin;
+									break;
+								}
+								var nextKyokumen = new Kyokumen(history[history.Count - 1], move);
+								if (CheckRepetition(nextKyokumen, history)) {
+									result = Result.Repetition;
+									break;
+								}
+								if (CheckEndless(history.Count)) {
+									result = Result.Draw;
+									break;
+								}
+								kifu.Add(move);
+								evals.Add(eval);
+								history.Add(nextKyokumen);
+							}
+							else {//後手
+								var (move, eval) = GetMove(gote, position(startsfen, kifu), go);
+								Console.Write($" w:{move}({-eval})");
+								if (move == "resign") {
+									result = Result.SenteWin;
+									break;
+								}
+								else if (move == "win") {
+									result = Result.GoteWin;
+									break;
+								}
+								var nextKyokumen = new Kyokumen(history[history.Count - 1], move);
+								if (CheckRepetition(nextKyokumen, history)) {
+									result = Result.Repetition;
+									break;
+								}
+								if (CheckEndless(history.Count)) {
+									result = Result.Draw;
+									break;
+								}
+								kifu.Add(move);
+								evals.Add(-eval);
+								history.Add(nextKyokumen);
+							}
+						}
+						Kifu.FoutKifu(starttime, matchname, b, w, byoyomi, kifu, startmove, evals, result, kifupath);
+						sendGameOver(sente, gote, result);
+						_kifu = kifu;
+						_evals = evals;
+						return result;
+					}
+					catch (Exception e) {
+						Console.WriteLine(e);
+						if (!sente.HasExited) sente.StandardInput.WriteLine("quit");
+						if (!gote.HasExited) gote.StandardInput.WriteLine("quit");
+					}
+					finally {
+						if (!sente.WaitForExit(100)) {
+							try {
+								sente.Kill();
+							}
+							catch (Exception) {
 								//プロセスは既に終了しているので何もしない（waitが終わってkillを呼ぶまでの一瞬の間にプロセスが終了した場合に例外が発生する）
 							}
 						}
@@ -220,6 +340,26 @@ namespace USI_MultipleMatch
 					gote.StandardInput.WriteLine("quit");
 					break;
 			}
+		}
+		static void sendOnlyGameOver(Process sente, Process gote, Result result) {
+			switch (result) {
+				case Result.SenteWin:
+					sente.StandardInput.WriteLine("gameover win");
+					gote.StandardInput.WriteLine("gameover lose");
+					break;
+				case Result.GoteWin:
+					sente.StandardInput.WriteLine("gameover lose");
+					gote.StandardInput.WriteLine("gameover win");
+					break;
+				case Result.Draw:
+				case Result.Repetition:
+					sente.StandardInput.WriteLine("gameover draw");
+					gote.StandardInput.WriteLine("gameover draw");
+					break;
+			}
+		}
+		static void sendQuit(Process engine) {
+			engine.StandardInput.WriteLine("quit");
 		}
 	}
 }
